e9ca8e7ab1c77902ecb141959e2da0e657133a6a
luisan00 <luisan00@laptop.home>
2021-01-27 16:18:29 +0100

    update listeners

--- html/js/app.js
+++ html/js/app.js
@@ -1,26 +1,33 @@
 const ti_bootloader = require("ti-bootloader");
 const usb = require("usb");
 
+
 var app = new Vue({
     el: "#app",
     data: {
-        ports: [],
+        ports: null,
         hotplugged: null
     },
     methods: {
-        selftDetection: function(){
+        selftDetection: function () {
+
             self = this;
-            usb.on('attach', function (dev) { 
+
+            usb.on('attach', function (dev) {
                 self.hotplugged = dev;
-                self.notify_open("info", "USB", "Device Connected")
+                // self.notify_open("info", "USB", "Device Connected");
+                self.listPorts();
             });
-            usb.on(`detach`, function(dev) {
+            usb.on(`detach`, function (dev) {
                 self.hotplugged = null;
-                self.notify_open("info", "USB", "Device Disconnected")
-            })
+                // self.notify_open("info", "USB", "Device Disconnected");
+                self.listPorts();
+            });
+
+
         },
         listPorts: function () {
-            this.ports = ti_bootloader.listSerialPorts();
+            this.ports = ti_bootloader.listSerialPorts() || [];
         },
         notify_open: function (type, title, message, duration) {
             this.$notify({
@@ -34,13 +41,25 @@ var app = new Vue({
     watch: {
         ports: {
             handler: function (n, o) {
-                console.log(n, o)
+                
+
+                if (n.length > 0) {
+                    var notifyPorts = []
+                    n.forEach(element => {
+                        console.log(element);
+                        notifyPorts.push(element.port)
+                        
+                    });
+                    self.notify_open("info", "USB", `${notifyPorts}`);
+                   
+                }
+
             },
             deep: true
         },
         hotplugged: {
             handler: function (n, o) {
-                
+                //
             },
             deep: true
         }
@@ -49,12 +68,12 @@ var app = new Vue({
         //
     },
     mounted: function () {
-        this.notify_open('success', 'ready', 'ready');
+        //this.notify_open('success', 'ready', 'ready');
         this.selftDetection();
 
     },
     updated: function () {
-        this.listPorts();
+        //this.listPorts();
     }
 
 })
