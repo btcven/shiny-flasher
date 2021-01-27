8b0bff495975f01f78835de86e3e1d2ad54a2afb
luisan00 <luisan00@laptop.home>
2021-01-27 15:32:18 +0100

    fix build and install scripts

--- html/js/app.js
+++ html/js/app.js
@@ -1,27 +1,60 @@
+const ti_bootloader = require("ti-bootloader");
+const usb = require("usb");
+
 var app = new Vue({
     el: "#app",
-    data: {},
+    data: {
+        ports: [],
+        hotplugged: null
+    },
     methods: {
+        selftDetection: function(){
+            self = this;
+            usb.on('attach', function (dev) { 
+                self.hotplugged = dev;
+                self.notify_open("info", "USB", "Device Connected")
+            });
+            usb.on(`detach`, function(dev) {
+                self.hotplugged = null;
+                self.notify_open("info", "USB", "Device Disconnected")
+            })
+        },
+        listPorts: function () {
+            this.ports = ti_bootloader.listSerialPorts();
+        },
         notify_open: function (type, title, message, duration) {
             this.$notify({
                 type: type,
                 title: title,
                 message: message,
-                duration: duration | 4600 
+                duration: duration | 4600
             });
         }
     },
     watch: {
-        //
+        ports: {
+            handler: function (n, o) {
+                console.log(n, o)
+            },
+            deep: true
+        },
+        hotplugged: {
+            handler: function (n, o) {
+                
+            },
+            deep: true
+        }
     },
     created: function () {
         //
     },
     mounted: function () {
         this.notify_open('success', 'ready', 'ready');
+        this.selftDetection();
+
     },
     updated: function () {
-
+        this.listPorts();
     }
 
 })
