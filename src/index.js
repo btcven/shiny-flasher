0d069601171a80a632d5d14178e80f06ed4ab278
xkevin190 <kevinvelasco193@gmail.com>
2021-01-27 19:21:40 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
+import store from './store'
+import { Provider } from 'react-redux'
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div')
@@ -9,4 +11,9 @@ root.id = 'root'
 document.body.appendChild(root)
 
 // Now we can render our application into it
-render(<App />, document.getElementById('root'))
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root'))
